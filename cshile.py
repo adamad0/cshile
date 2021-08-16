7645ae2cf0823fd623e749a07086d86d17f6305d
adamad0 <m.samikarakelleoglu@gmail.com>
2021-08-16 20:42:39 +0300

    Create cshile.py

---
+++ cshile.py
@@ -0,0 +1,45 @@
+import pymem
+import pymem.process
+
+
+dwEntityList = (0x4DA2F54)
+dwLocalPlayer = (0xD8B2CC)
+dwGlowObjectManager = (0x52EB548)
+m_iGlowIndex = (0xA438)
+m_iTeamNum = (0xF4) 
+
+
+def main():
+    print("adamado hilesi aktiveytıd")
+    pm = pymem.Pymem("csgo.exe")
+    client = pymem.process.module_from_name(pm.process_handle,"client.dll").lpBaseOfDll
+    
+    while True:
+
+        
+        glow_manager = pm.read_int(client + dwGlowObjectManager)
+        for i in range(1,30):
+                entity = pm.read_int(client + dwEntityList + i * 0x10)
+                
+                
+                if entity:
+                    entity_team_id = pm.read_int(entity + m_iTeamNum)
+                    entity_glow = pm.read_int(entity + m_iGlowIndex)
+                        
+                    if entity_team_id == (2):
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(1))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(0))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(1))
+                        pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)
+                   
+                    elif entity_team_id == (3):
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(0))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(1))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(1))
+                        pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)
+                    
+                    
+if  __name__ == "__main__":
+    main()
